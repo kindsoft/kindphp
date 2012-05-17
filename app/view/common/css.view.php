b8fa2d9f0d00f7c7d26b4710b277e5d6eb9f430a
Longhao Luo <luolonghao@gmail.com>
2012-05-17 15:15:30 +0800

    Remove bootstrap-responsive

--- app/view/common/css.view.php
+++ app/view/common/css.view.php
@@ -1,5 +1,4 @@
 <?php KindPHP::css('/bootstrap/css/bootstrap.min.css'); ?>
-<?php KindPHP::css('/bootstrap/css/bootstrap-responsive.min.css'); ?>
 <?php KindPHP::css('/bootstrap/css/chinese.css'); ?>
 <!--[if IE 6]>
 <?php KindPHP::css('/bootstrap/css/ie6.css'); ?>

8cba480f1131ecdff18e370dda2e53083d2d6a4d
Longhao Luo <luolonghao@gmail.com>
2012-07-26 14:33:16 +0800

    Replace bootstrap ie6, delete debug files

--- app/view/common/css.view.php
+++ app/view/common/css.view.php
@@ -1,8 +1,4 @@
 <?php KindPHP::css('/bootstrap/css/bootstrap.min.css'); ?>
-<?php KindPHP::css('/bootstrap/css/chinese.css'); ?>
 <!--[if IE 6]>
-<?php KindPHP::css('/bootstrap/css/ie6.css'); ?>
+<?php KindPHP::css('/bootstrap/css/bootstrap-ie6.css'); ?>
 <![endif]-->
-<style>
-	body {margin: 5px 0;}
-</style>
--- app/view/common/js.view.php
+++ app/view/common/js.view.php
@@ -2,7 +2,4 @@
 <?php KindPHP::js('/bootstrap/js/html5.js'); ?>
 <![endif]-->
 <?php KindPHP::js('/jquery/jquery.min.js'); ?>
-<!--[if IE 6]>
-<?php KindPHP::js('/bootstrap/js/ie6.js'); ?>
-<![endif]-->
 <?php KindPHP::js('/bootstrap/js/bootstrap.min.js'); ?>
